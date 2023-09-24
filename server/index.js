b2198c5f5de6914531aee81a0cb4cdd67530fa4b
Punnoose Punnen <punnoose88@gmail.com>
2023-09-24 13:45:03 +0530

    backend

--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ app.use(
     credentials: true,
   })
 );
+
+
 app.use(express.json());
 
 app.use('/api/auth', userRoute);
@@ -29,11 +31,11 @@ mongoose
   })
   .catch((err) => console.logn(err.message));
 
-// const server = app.listen(process.env.PORT, () =>
-//   console.log(`connected to port ${process.env.PORT}`)
-// );
+const server = app.listen(process.env.PORT, () =>
+  console.log(`connected to port ${process.env.PORT}`)
+);
 
-const server = 'https://chat-mern-api.vercel.app/';
+// const server = 'https://chat-mern-api.vercel.app';
 
 const io = socket(server, {
   cors: {
